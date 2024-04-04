ff0464d8ec21d0862c5ae2936112d1dca6e85008
Rati Krishna Moharana <73690698+rkmoharana-cyber@users.noreply.github.com>
2024-04-04 21:11:26 +0530

    Required Files Added

---
+++ Todo.js
@@ -0,0 +1,42 @@
+let inputBox =  document.querySelector("#input-box");
+let button = document.querySelector("button");
+let listContainer = document.querySelector("#list-container");
+
+inputBox.addEventListener("keypress", function(event) {
+    if (event.key === "Enter") {
+        button.click();
+    }
+})
+button.addEventListener("click", function() {
+    if (inputBox.value === "") {
+        alert("You must write something!");
+    }
+    else {
+        let li = document.createElement("li");
+        li.innerText = inputBox.value;
+        listContainer.appendChild(li);
+        let cross = document.createElement("span");
+        cross.innerText = "\u00d7";
+        li.appendChild(cross);
+    }
+    inputBox.value = "";
+    saveData();
+});
+
+listContainer.addEventListener("click", function(event) {
+    if (event.target.tagName === "LI") {
+        event.target.classList.toggle("checked");
+        saveData();
+    } else if (event.target.tagName === "SPAN") {
+        event.target.parentElement.remove();
+        saveData();
+    }
+},false);
+
+function saveData() {
+    localStorage.setItem("krishnaData", listContainer.innerHTML);
+}
+function showData() {
+    listContainer.innerHTML = localStorage.getItem("krishnaData");
+}
+showData();
